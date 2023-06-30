2aade37368a22080e4ecdbb445a66d2221949d58
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-30 15:54:01 +0300

    create structure and update script.js

---
+++ script.js
@@ -0,0 +1,66 @@
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.topping = [];
+  }
+  // TODO: Size
+  static SIZE_SMALL = { price: 50, calories: 20 };
+  static SIZE_BIG = { price: 100, calories: 40 };
+
+  // TODO: Stuffing
+  static STUFFING_CHEESE = { price: 10, calories: 20 };
+  static STUFFING_SALAD = { price: 20, calories: 5 };
+  static STUFFING_POTATO = { price: 15, calories: 10 };
+
+  // TODO: Topping
+  static TOPPING_MAYO = { price: 20, calories: 5 };
+  static TOPPING_SAUCE = { price: 15, calories: 0 };
+
+  addTopping(topping) {
+    this.topping.push(topping);
+  }
+
+  calculate() {
+    const baseCalories = this.size.calories + this.stuffing.calories;
+    const toppingCalories = this.topping.reduce(
+      (total, topping) => total + topping.calories,
+      0
+    );
+
+    return baseCalories + toppingCalories;
+  }
+
+  calculatePrice() {
+    const basePrice = this.size.price + this.stuffing.price;
+    const toppingPrice = this.topping.reduce(
+      (total, toping) => total + toping.price,
+      0
+    );
+
+    return basePrice + toppingPrice;
+  }
+}
+
+// маленький гамбургер з начинкою з сиру
+const hamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE
+);
+console.log(hamburger);
+// добавка з майонезу
+hamburger.addTopping(Hamburger.TOPPING_MAYO);
+
+// запитаємо скільки там калорій
+console.log("Calories: " + hamburger.calculate());
+
+//  скільки коштує
+console.log("Price: " + hamburger.calculatePrice());
+
+// я тут передумав і вирішив додати ще приправу
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+
+// А скільки тепер коштує?
+console.log("Price with sauce: " + hamburger.calculatePrice());
+// А скільки тепер калорій?
+console.log("Calories with sauce: " + hamburger.calculate());
